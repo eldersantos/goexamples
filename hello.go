5bc8fd8c4ad795833fb794c038fcd728e7e7504c
Elder Santos <elder.santos@gmail.com>
2020-12-04 19:01:19 -0300

    changes

--- hello.go
+++ hello.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 const name = "John Doe"
 
 func main() {
 	fmt.Println("Hello World 的", name)
+	for _, arg := range os.Args[1:] {
+		fmt.Println(arg)
+	}
+
+	var x = 0
+
+	for {
+		x++
+		if x == 10 {
+			break
+		}
+		fmt.Println(x)
+	}
+
+	fmt.Println(strings.Join(os.Args[1:], ":"))
 }
