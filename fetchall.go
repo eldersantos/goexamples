e41dddd2c8a6c5e845f393c4418005264d1fb297
Elder Santos <elder.santos@gmail.com>
2021-01-14 11:32:56 -0300

    changes

--- fetchall.go
+++ fetchall.go
@@ -10,9 +10,16 @@ import (
 	"time"
 )
 
+// type for urlcounts
+type UrlCounts struct {
+	Message string
+	Error   int
+	Success int
+}
+
 func main() {
 	start := time.Now()
-	ch := make(chan string)
+	ch := make(chan UrlCounts)
 
 	f, err := ioutil.ReadFile("urls.txt")
 	if err != nil {
@@ -23,28 +30,33 @@ func main() {
 	sliceData := strings.Split(string(f), "\n")
 
 	for _, url := range sliceData {
+		if !strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
 		go fetch(url, ch) // start a goroutine
 	}
 	for range sliceData {
-		fmt.Println(<-ch) // receive from channel ch
+		fmt.Println(<-ch.Message) // receive from channel ch
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
-func fetch(url string, ch chan<- string) {
+func fetch(url string, ch chan<- UrlCounts) {
 	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
-		ch <- fmt.Sprint(err) // send to channel ch
+		ch <- UrlCounts{fmt.Sprint(err), ch.Error + 1, ch.Success + 1}
 		return
 	}
 
 	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close() // don't leak resources
 	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		ch.Message <- fmt.Sprintf("while reading %s: %v", url, err)
+		ch.Error++
 		return
 	}
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+	ch.Message <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+	ch.Success++
 }
